881910efa4522ab5fe1c9641f51739feee15aa6b
dylanon <on.dylan@gmail.com>
2016-05-11 23:38:48 -0400

    Starting point - add original files via upload
    Original theme files as they were when I started using GitHub. Menu bars are functional and the blog view displays correctly in Bootstrap's grid.

---
+++ taylorhammond/functions.php
@@ -0,0 +1,14 @@
+<?php
+
+function taylorhammond_enqueue_style() {
+	wp_enqueue_style( 'core', get_stylesheet_uri(), false );
+}
+
+function taylorhammond_enqueue_script() {
+	wp_enqueue_script( 'bootstrap-js', get_template_directory_uri() . '/js/bootstrap.min.js', array( 'jquery' ), true );
+}
+
+add_action( 'wp_enqueue_scripts', 'taylorhammond_enqueue_style' );
+add_action( 'wp_enqueue_scripts', 'taylorhammond_enqueue_script' );
+
+?>
