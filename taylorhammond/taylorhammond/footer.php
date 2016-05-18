45fce7ac5a38692523adefb9a134875c54a30788
dylanon <on.dylan@gmail.com>
2016-05-18 15:27:36 -0400

    More styling & font changes
    - Global styles for paragraphs and headings
    - Adjusted post padding, heading and date font size in archive view
    - Changed heading font to Roboto Slab, imported from Google Fonts
    - Modified backup fonts for paragraphs to improve compatibility

---
+++ taylorhammond/taylorhammond/footer.php
@@ -0,0 +1,10 @@
+  <div class="footer text-center small">
+    
+    <p>
+      &copy;&nbsp;<?php echo date("Y") ?> Taylor Hammond
+    </p>
+    
+  </div>
+  <?php wp_footer(); ?>
+  </body>
+</html>
---
+++ taylorhammond/taylorhammond/functions.php
@@ -0,0 +1,20 @@
+<?php
+
+function taylorhammond_enqueue_style() {
+	wp_enqueue_style( 'core', get_stylesheet_uri() );
+}
+
+function taylorhammond_enqueue_script() {
+	wp_enqueue_script( 'bootstrap-js', get_template_directory_uri() . '/js/bootstrap.min.js', array( 'jquery' ), true );
+}
+
+add_action( 'wp_enqueue_scripts', 'taylorhammond_enqueue_style' );
+add_action( 'wp_enqueue_scripts', 'taylorhammond_enqueue_script' );
+
+function custom_theme_setup() {
+	add_theme_support( 'title-tag' );
+}
+
+add_action( 'after_setup_theme', 'custom_theme_setup' );
+
+?>
