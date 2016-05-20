9c88e8636a9127b71e1657ed8b16ad6065cf34a4
dylanon <on.dylan@gmail.com>
2016-05-20 17:58:15 -0400

    Styled feat image caption. Visual editor style works but does not display in editor itself.

--- functions.php
+++ functions.php
@@ -29,4 +29,34 @@ function the_post_thumbnail_caption() {
   }
 }
 
+// Add Styleselect functionality to visual post editor
+// Callback function to insert 'styleselect' into the $buttons array
+function my_mce_buttons_2( $buttons ) {
+	array_unshift( $buttons, 'styleselect' );
+	return $buttons;
+}
+// Register our callback to the appropriate filter
+add_filter( 'mce_buttons_2', 'my_mce_buttons_2' );
+
+// Register custom styles in visual post editor
+// Callback function to filter the MCE settings
+function my_mce_before_init_insert_formats( $init_array ) {  
+	// Define the style_formats array
+	$style_formats = array(  
+		// Each array child is a format with it's own settings
+		array(  
+			'title' => 'Project Info',
+			'inline' => 'span',
+			'classes' => 'portfolio-info',
+		),  
+	);  
+	// Insert the array, JSON ENCODED, into 'style_formats'
+	$init_array['style_formats'] = json_encode( $style_formats );  
+	
+	return $init_array;  
+  
+} 
+// Attach callback to 'tiny_mce_before_init' 
+add_filter( 'tiny_mce_before_init', 'my_mce_before_init_insert_formats' );  
+
 ?>
