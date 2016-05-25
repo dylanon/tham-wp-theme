9e884b80f9c83b74b9cfea562e4ca12b1d8da7ca
dylanon <on.dylan@gmail.com>
2016-05-24 22:35:55 -0400

    Hooked up templates to Performance master navbar

--- page.php
+++ page.php
@@ -1,36 +1,6 @@
 <?php get_header(); ?>
 
-    <nav class="navbar navbar-default text-uppercase">
-      <div class="container">
-
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="#"><img class="taylor-brand" src="<?php bloginfo('template_directory'); ?>/img/logo-white.png" alt="Taylor Hammond" /></a>
-          
-        </div>
-
-        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-          <ul class="nav navbar-nav nav-links navbar-right">
-            <li><a href="#">Past</a></li>
-            <li class="active"><a href="#">Present</a></li>
-            <li><a href="#" class="hide-crossover">Thought</a></li>
-          </ul>
-           <h1 class="section-heading navbar-right">Performance</h1>
-        </div>
-
-      </div>
-    </nav>
-    
-    <div class="container-fluid title-block">
-      <h1 class="text-uppercase text-center">
-        Performance
-      </h1>
-    </div>
+<?php get_template_part( 'nav-performance' ); // Navigation bar ?>
 
     <div class="page-width container">
       
