af77f77407de7ee97510bcf5d55ef08a0fcbd31c
dylanon <on.dylan@gmail.com>
2016-05-24 21:51:38 -0400

    Added blank page templates (no titles). Adjusted navbar bottom margin and content top margin as needed.

---
+++ page-templates/performance-no-title.php
@@ -0,0 +1,49 @@
+<?php /* Template Name: Performance (No Title) */ ?>
+
+<?php get_header(); ?>
+
+    <nav class="navbar navbar-default text-uppercase">
+      <div class="container">
+
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#"><img class="taylor-brand" src="<?php bloginfo('template_directory'); ?>/img/logo-white.png" alt="Taylor Hammond" /></a>
+          
+        </div>
+
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          <ul class="nav navbar-nav nav-links navbar-right">
+            <li><a href="#">Past</a></li>
+            <li class="active"><a href="#">Present</a></li>
+            <li><a href="#" class="hide-crossover">Thought</a></li>
+          </ul>
+           <h1 class="section-heading navbar-right">Performance</h1>
+        </div>
+
+      </div>
+    </nav>
+    
+    <div class="container-fluid title-block">
+      <h1 class="text-uppercase text-center">
+        Performance
+      </h1>
+    </div>
+
+    <div class="page-width container">
+      
+      <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+        <?php the_content(); ?>
+        <?php edit_post_link(); ?>
+
+      <?php endwhile; else: ?>
+        <p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
+      <?php endif; ?>  
+
+    </div> <!-- .container -->
+<?php get_footer(); ?>
