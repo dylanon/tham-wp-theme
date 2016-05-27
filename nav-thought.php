876890c476ac7d3f4a036c6d6b661ab83ef3d40a
dylanon <on.dylan@gmail.com>
2016-05-27 15:26:17 -0400

    Implemented crossover for Thought side & shortened fade times

--- nav-thought.php
+++ nav-thought.php
@@ -27,7 +27,7 @@
               <li><a href="<?php get_bloginfo( ‘template_url’ ); ?>/present" class="hide-crossover">Performance</a></li>
             </ul>
           </div>
-           <h1 class="section-heading navbar-right">Thought</h1>
+           <h1 class="section-heading navbar-right" id="nav-crossover-trigger">Thought</h1>
         </div>
 
       </div>
@@ -38,3 +38,33 @@
         Thought
       </h1>
     </div>
+
+<div id="nav-crossover">
+  
+   <nav class="navbar navbar-default text-uppercase">
+    <div class="container">
+
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="<?php echo get_home_url(); ?>"><img class="taylor-brand" src="<?php bloginfo('template_directory'); ?>/img/logo-white.png" alt="Taylor Hammond" /></a>
+
+      </div>
+
+      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <ul class="nav navbar-nav nav-links navbar-right">
+          <li <?php if ( $pagename == 'past' or $mysection == 'past' ) { ?>class="active"<?php } ?>><a href="<?php get_bloginfo( ‘template_url’ ); ?>/past">Past</a></li>
+          <li <?php if ( $pagename == 'present' ) { ?>class="active"<?php } ?>><a href="<?php get_bloginfo( ‘template_url’ ); ?>/present">Present</a></li>
+          <li><a href="<?php get_bloginfo( ‘template_url’ ); ?>/category/blog" class="hide-crossover">Thought</a></li>
+        </ul>
+         <h1 class="section-heading navbar-right" id="nav-crossover-trigger">Performance</h1>
+      </div>
+
+    </div>
+  </nav>
+  
+</div>
