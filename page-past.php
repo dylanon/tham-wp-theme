2696528ba7cc46e6fbf3b50445eef1a49a3fae8c
dylanon <on.dylan@gmail.com>
2016-05-31 17:48:33 -0400

    Fix page title with .no-bottom-margin

--- page-past.php
+++ page-past.php
@@ -6,7 +6,7 @@
       
       <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-        <h1 class="text-center"><?php the_title(); ?> Projects</h1>
+        <h1 class="text-center no-bottom-margin"><?php the_title(); ?> Projects</h1>
       
         <?php if ( has_post_thumbnail() ) { ?>
 
@@ -19,93 +19,93 @@
 
         <?php } ?>
       
-        <div class="row">
-          
-          <div class="col-sm-6 square-size">
-            
-            <a href="<?php get_bloginfo( ‘template_url’ ); ?>/portfolio_category/actor">
-              
-              <div class="square border-yellow border-rounded">
+          <div class="row">
 
-                <div class="square-content square-background">
+            <div class="col-sm-6 square-size">
 
-                  <h2 class="h1">
-                    Actor
-                  </h2>
+              <a href="<?php get_bloginfo( ‘template_url’ ); ?>/portfolio_category/actor">
 
-                </div>
+                <div class="square border-yellow border-rounded">
 
-              </div>
-              
-            </a>
-            
-          </div>
-          
-          <div class="col-sm-6 square-size">
-            
-            <a href="<?php get_bloginfo( ‘template_url’ ); ?>/portfolio_category/creator">
-              
-              <div class="square border-yellow border-rounded">
+                  <div class="square-content square-background">
 
-                <div class="square-content square-background">
+                    <h2 class="h1">
+                      Actor
+                    </h2>
 
-                  <h2 class="h1">
-                    Creator
-                  </h2>
+                  </div>
 
                 </div>
 
-              </div>
-              
-            </a>
-            
-          </div>
-          
-        </div><!-- .row -->
+              </a>
 
-        <div class="row">
-          
-          <div class="col-sm-6 square-size">
-            
-            <a href="<?php get_bloginfo( ‘template_url’ ); ?>/portfolio_category/director">
-              
-              <div class="square border-yellow border-rounded">
+            </div>
 
-                <div class="square-content square-background">
+            <div class="col-sm-6 square-size">
 
-                  <h2 class="h1">
-                    Director
-                  </h2>
+              <a href="<?php get_bloginfo( ‘template_url’ ); ?>/portfolio_category/creator">
+
+                <div class="square border-yellow border-rounded">
+
+                  <div class="square-content square-background">
+
+                    <h2 class="h1">
+                      Creator
+                    </h2>
+
+                  </div>
 
                 </div>
 
-              </div>
-              
-            </a>
-            
-          </div>
-          
-          <div class="col-sm-6 square-size">
-            
-            <a href="<?php get_bloginfo( ‘template_url’ ); ?>/portfolio_category/teacher">
-              
-              <div class="square border-yellow border-rounded">
+              </a>
 
-                <div class="square-content square-background">
+            </div>
 
-                  <h2 class="h1">
-                    Teacher
-                  </h2>
+          </div><!-- .row -->
+
+          <div class="row">
+
+            <div class="col-sm-6 square-size">
+
+              <a href="<?php get_bloginfo( ‘template_url’ ); ?>/portfolio_category/director">
+
+                <div class="square border-yellow border-rounded">
+
+                  <div class="square-content square-background">
+
+                    <h2 class="h1">
+                      Director
+                    </h2>
+
+                  </div>
 
                 </div>
 
-              </div>
-              
-            </a>
-            
-          </div>
-          
-        </div><!-- .row -->
+              </a>
+
+            </div>
+
+            <div class="col-sm-6 square-size">
+
+              <a href="<?php get_bloginfo( ‘template_url’ ); ?>/portfolio_category/teacher">
+
+                <div class="square border-yellow border-rounded">
+
+                  <div class="square-content square-background">
+
+                    <h2 class="h1">
+                      Teacher
+                    </h2>
+
+                  </div>
+
+                </div>
+
+              </a>
+
+            </div>
+
+          </div><!-- .row -->
       
         <div class="top-margin"><?php the_content(); ?></div>
         <?php edit_post_link(); ?>
