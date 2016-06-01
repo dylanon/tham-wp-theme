033ed1e53953714269236d9ed88f9796e70223b8
dylanon <on.dylan@gmail.com>
2016-05-31 20:13:25 -0400

    Exploratory javascript...

--- js/nav-crossover.js
+++ js/nav-crossover.js
@@ -1,11 +1,20 @@
 jQuery(document).ready(function($){ /* WordPress loads jQuery in no conflict mode. $ is passed back in here for use within the script. */
-
+  
+  var crossClicked = false;
+  
   $("#nav-crossover-trigger").mouseenter(function(){
-    $("#nav-crossover").fadeIn(300);
+    if (crossClicked === false) {
+      $("#nav-crossover").fadeIn(300);
+    }
   });
   
   $("#nav-crossover").mouseleave(function(){
     $("#nav-crossover").fadeOut(300);
+  });
+  
+  $("#nav-crossover").click(function(){
+    $("#nav-crossover").fadeOut(300);
+    crossClicked = true;
   });
 
 });
