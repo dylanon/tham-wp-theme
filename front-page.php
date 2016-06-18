cc4bb522211bd3a64960bfee74eec2d54fd22c6a
dylanon <on.dylan@gmail.com>
2016-06-18 19:21:29 -0400

    Positioning implemented

--- front-page.php
+++ front-page.php
@@ -3,7 +3,7 @@
 <div id="front-page-performance" class="flex-center-contents">
   
   <a href="#">
-    <div class="text-uppercase">
+    <div class="text-uppercase text-center front-page-center-links front-page-performance">
       <h1>Performance</h1>
     </div>
   </a>
@@ -13,7 +13,7 @@
 <div id="front-page-thought" class="flex-center-contents">
   
   <a href="#">
-    <div class="text-uppercase">
+    <div class="text-uppercase text-center front-page-center-links front-page-thought">
       <h1>Thought</h1>
     </div>
   </a>
