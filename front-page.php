e87ee52065699f7a5c67eb47d5e3cca3dd82f103
dylanon <on.dylan@gmail.com>
2016-06-18 17:37:43 -0400

    Basic mobile layout for frontpage

--- front-page.php
+++ front-page.php
@@ -1,9 +1,23 @@
 <?php get_header(); ?>
 
-<div id="front-page-performance">
+<div id="front-page-performance" class="flex-center-contents">
+  
+  <a href="#">
+    <div class="text-uppercase">
+      <h1>Performance</h1>
+    </div>
+  </a>
+  
 </div>
 
-<div id="front-page-thought">
+<div id="front-page-thought" class="flex-center-contents">
+  
+  <a href="#">
+    <div class="text-uppercase">
+      <h1>Thought</h1>
+    </div>
+  </a>
+  
 </div>
 
 <?php get_footer(); ?>
