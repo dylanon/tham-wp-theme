442b8f8d7aead3d5a8d3a14b8e6ce997246ee454
on.dylan <on.dylan@gmail.com>
2016-12-13 22:07:32 -0500

    Start adding comments functionality to Reviews.

--- comments.php
+++ comments.php
@@ -20,31 +20,11 @@ if ( post_password_required() ) {
 }
 ?>
 
+<hr />
 <div id="comments" class="comments-area">
 
 	<?php if ( have_comments() ) : ?>
-		<h2 class="comments-title">
-			<?php
-				$comments_number = get_comments_number();
-				if ( 1 === $comments_number ) {
-					/* translators: %s: post title */
-					printf( _x( 'One thought on &ldquo;%s&rdquo;', 'comments title', 'twentysixteen' ), get_the_title() );
-				} else {
-					printf(
-						/* translators: 1: number of comments, 2: post title */
-						_nx(
-							'%1$s thought on &ldquo;%2$s&rdquo;',
-							'%1$s thoughts on &ldquo;%2$s&rdquo;',
-							$comments_number,
-							'comments title',
-							'twentysixteen'
-						),
-						number_format_i18n( $comments_number ),
-						get_the_title()
-					);
-				}
-			?>
-		</h2>
+		<h2 class="comments-title text-center">Comments</h2>
 
 		<?php the_comments_navigation(); ?>
 
@@ -53,7 +33,8 @@ if ( post_password_required() ) {
 				wp_list_comments( array(
 					'style'       => 'ol',
 					'short_ping'  => true,
-					'avatar_size' => 42,
+					'avatar_size' => 21, // Originally 42
+					'reverse_top_level' => true, // Show comments in reverse chronological order
 				) );
 			?>
 		</ol><!-- .comment-list -->
