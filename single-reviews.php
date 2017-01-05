50b20bd881be6b7a987d5a58446d80cf4d5bbfee
on.dylan <on.dylan@gmail.com>
2017-01-04 21:32:09 -0500

    Remove single-reviews.php and single_master.php because unnecessary

--- single-reviews.php
+++
@@ -1,9 +0,0 @@
-<?php get_header(); ?>
-
-<?php $mysection = 'reviews'; ?>
-
-<?php get_template_part( 'nav-thought' ); // Navigation bar ?>
-
-<?php get_template_part( 'single_master' ); // Identical single post view code for Blog posts and Reviews ?>
-
-<?php get_footer(); ?>
--- single.php
+++ single.php
@@ -4,6 +4,43 @@
 
 <?php get_template_part( 'nav-thought' ); // Navigation bar ?>
 
-<?php get_template_part( 'single_master' ); // Identical single post view code for Blog posts and Reviews ?>
+<div class="bottom-padded slim-width container">
+
+	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+	<div class="blog-post">
+
+		<h1 class="text-uppercase"><?php the_title(); ?></h1>
+		
+		<?php if ( has_post_thumbnail() ) { ?>
+		
+		<div class="featured-image featured-image-blog-larger-devices">
+			<?php the_post_thumbnail( 'full', array( 'class' => 'border-yellow' ) ) ?>
+			<div class="featured-image-caption">
+				<?php the_post_thumbnail_caption() ?>
+			</div>
+		</div>
+		
+		<?php } ?>
+		
+		<span class="post-wrapper"><?php the_content('(Read more ...)'); ?></span>
+		<p class="small text-right text-uppercase img-floats-fix"><br />
+			<?php the_tags( 'Tagged in: ' ); ?><br />
+			<?php the_time('l F j, Y'); ?><br /><br />
+			<strong><?php edit_post_link( '[ Edit This ]' ); ?></strong>
+		</p>
+		<hr />
+		<?php comments_template(); ?>
+
+	</div> <!-- .blog-post -->
+
+	<?php endwhile; else : ?>
+			<p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+	<?php endif; ?>
+
+	<div class="text-center"><strong><?php next_post_link( '%link', '&#8592; %title (newer)' ); ?>&nbsp;&nbsp;//&nbsp;&nbsp;<?php previous_post_link(  '%link', '%title (older) &#8594;' ); ?></strong></div>
+	<hr />
+	
+</div>
 
 <?php get_footer(); ?>
