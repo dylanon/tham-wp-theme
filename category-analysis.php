b758a86cad1d8a3e688c3639772c5df568458c24
on.dylan <on.dylan@gmail.com>
2017-01-11 21:32:31 -0500

    Pull Analysis page text from specific post via ID. Mobile/tablet only.

--- category-analysis.php
+++ category-analysis.php
@@ -6,8 +6,14 @@
 
     <div class="bottom-padded container">
       
-      <h1 class="text-center mobile-only">Analysis</h1>
-
+      <h1 class="text-center mobile-tablet-only">Analysis</h1>
+      
+      <div class="text-center text-byte mobile-tablet-only">
+        <hr />
+        <p><?php echo get_the_excerpt( '265' ); ?></p> <!-- Change this ID to match the 'Why Analysis' post on install. -->
+        <hr />      
+      </div>
+      
       <?php get_template_part( 'master_posts-view' ); // Posts View master code for category, archive,
                                                       // and portfolio category pages ?>
 
