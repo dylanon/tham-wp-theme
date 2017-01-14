132765b69f7d1b4bee8c43a374989f75227ffb58
on.dylan <on.dylan@gmail.com>
2017-01-14 15:05:00 -0500

    Portfolio posts content ordering. Title displays at top on mobile now.

--- page-templates/portfolio-portrait-image.php
+++ page-templates/portfolio-portrait-image.php
@@ -15,54 +15,56 @@ Template Post Type: portfolio
 	<!-- Start the WordPress Loop -->
 	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 	
-			<div class="portfolio-post">
+		<div class="portfolio-post">
 
-				<div class="row">
-			
-					<?php if ( has_post_thumbnail() ) { ?>
-			
-						<!-- Set left box (featured image) width to 5/12 -->
-						<div class="col-sm-5 featured-image">
-							<?php the_post_thumbnail( 'full', array( 'class' => 'border-red' ) ) ?>
-							<div class="featured-image-caption">
-								<?php the_post_thumbnail_caption() ?>
-							</div>
-							<!-- Meta Box: Beneath featured image -->
-							<div class="portfolio-meta-box">
-								<?php echo do_shortcode( rwmb_meta( 'portfolio-customfield-left' ) ); ?>
-							</div>
-						</div>
-			
-						<!-- Set right box (content) width to 7/12 -->
-						<div class="col-sm-7">
-				
-					<?php } else { ?>
-			
-						<!-- Set right box (content) width to full width -->
-						<div class="col-sm-12"><!-- If no featured image -->
-			
-					<?php } ?>
-			
-							<div class="portfolio-content-area">
+			<div class="row">
 
-								<h1 class="text-uppercase"><?php the_title(); ?></h1>
-								<span class="post-wrapper"><?php the_content('(Read more ...)'); ?></span>
-								<p class="small text-right text-uppercase img-floats-fix"><br />
-									<strong><?php edit_post_link( '[ Edit This ]' ); ?></strong>
-								</p>
+				<?php if ( has_post_thumbnail() ) { ?>
 
-							</div>
+					<!-- Set right box (content) width to 7/12 -->
+					<div class="col-sm-7 col-sm-push-5">
 
-							<!-- Meta Box: Beneath main content area -->
-							<div class="portfolio-meta-box">
-								<?php echo do_shortcode( rwmb_meta( 'portfolio-customfield-right' ) ); ?>
-							</div>
-				
-						</div><!-- .col-sm-12 when there is no featured image -->
-		
-				</div><!-- .row -->
+				<?php } else { ?>
 
-			</div><!-- .portfolio-post -->
+					<!-- Set right box (content) width to full width -->
+					<div class="col-sm-12"><!-- If no featured image -->
+
+				<?php } ?>
+
+					<div class="portfolio-content-area">
+
+						<h1 class="text-uppercase"><?php the_title(); ?></h1>
+						<span class="post-wrapper"><?php the_content('(Read more ...)'); ?></span>
+						<p class="small text-right text-uppercase img-floats-fix"><br />
+							<strong><?php edit_post_link( '[ Edit This ]' ); ?></strong>
+						</p>
+
+					</div>
+
+					<!-- Meta Box: Beneath main content area -->
+					<div class="portfolio-meta-box">
+						<?php echo do_shortcode( rwmb_meta( 'portfolio-customfield-right' ) ); ?>
+					</div>
+
+					</div><!-- .col-sm-5 | .col-sm-12 when there is no featured image -->
+
+				<?php if ( has_post_thumbnail() ) { ?>
+				<!-- Set left box (featured image) width to 5/12 -->
+				<div class="col-sm-5 col-sm-pull-7 featured-image">
+					<?php the_post_thumbnail( 'full', array( 'class' => 'border-red' ) ) ?>
+					<div class="featured-image-caption">
+						<?php the_post_thumbnail_caption() ?>
+					</div>
+					<!-- Meta Box: Beneath featured image -->
+					<div class="portfolio-meta-box">
+						<?php echo do_shortcode( rwmb_meta( 'portfolio-customfield-left' ) ); ?>
+					</div>
+				</div><!-- .col-sm-7 -->
+				<?php } ?>
+
+			</div><!-- .row -->
+
+		</div><!-- .portfolio-post -->
 
 	<?php endwhile; else : ?>
 			<p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
