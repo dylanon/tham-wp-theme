e7021a8412641df2ab42805599fbad6696428d4f
on.dylan <on.dylan@gmail.com>
2017-01-22 17:28:46 -0500

    Portfolio nav structure first pass

--- taxonomy-portfolio_category-actor.php
+++ taxonomy-portfolio_category-actor.php
@@ -6,9 +6,31 @@
 
     <div class="bottom-padded container">
       
-      <h1 class="text-center">
+      <!-- Portfolio nav -->
+      <div class="row text-center">
+        <div class="col-xs-12 col-sm-2">
+          <p>
+            Projects as:
+          </p>
+        </div>
+        <div class="col-sm-2"></div>
+        <div class="col-xs-6 col-sm-2">
+          <a href="<?php get_bloginfo( ‘template_url’ ); ?>/past/actor">Actor</a>
+        </div>
+        <div class="col-xs-6 col-sm-2">
+          <a href="<?php get_bloginfo( ‘template_url’ ); ?>/past/creator">Creator</a>
+        </div>
+        <div class="col-xs-6 col-sm-2">
+          <a href="<?php get_bloginfo( ‘template_url’ ); ?>/past/director">Director</a>
+        </div>
+        <div class="col-xs-6 col-sm-2">
+          <a href="<?php get_bloginfo( ‘template_url’ ); ?>/past/teacher">Teacher</a>
+        </div>
+      </div>
+      
+<!--       <h1 class="text-center">
         Projects as Actor
-      </h1>
+      </h1> -->
       
       <?php get_template_part( 'master_posts-view' ); // Posts View master code for category, archive,
                                                       // and portfolio category pages ?>
