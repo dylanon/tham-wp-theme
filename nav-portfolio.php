6db8ab9af49f04940e7b6e78a6da64299dbedf1f
on.dylan <on.dylan@gmail.com>
2017-01-24 00:48:38 -0500

    Styling for desktop. Known bug re: posts views.

--- nav-portfolio.php
+++ nav-portfolio.php
@@ -4,28 +4,28 @@
     <div class="row text-center">
       <div class="col-xs-12 col-sm-4">
         <p>
-          View more projects:
+          View projects as:
         </p>
       </div>
 <!--       <div class="col-sm-2"></div> -->
       <div class="col-xs-6 col-sm-2<?php if ( has_term ( 'actor', 'portfolio_category' ) ) { ?> portfolio-active<?php } ?>">
         <a href="<?php get_bloginfo( ‘template_url’ ); ?>/past/actor">
-          Actor
+          Actor<?php if ( has_term ( 'actor', 'portfolio_category' ) === FALSE ) { ?> <span class="portfolio-arrow-fix">&#8594;</span><?php } ?>
         </a>
       </div>
       <div class="col-xs-6 col-sm-2<?php if ( has_term ( 'creator', 'portfolio_category' ) ) { ?> portfolio-active<?php } ?>">
         <a href="<?php get_bloginfo( ‘template_url’ ); ?>/past/creator">
-          Creator
+          Creator<?php if ( has_term ( 'creator', 'portfolio_category' ) === FALSE ) { ?> <span class="portfolio-arrow-fix">&#8594;</span><?php } ?>
         </a>
       </div>
       <div class="col-xs-6 col-sm-2<?php if ( has_term ( 'director', 'portfolio_category' ) ) { ?> portfolio-active<?php } ?>">
         <a href="<?php get_bloginfo( ‘template_url’ ); ?>/past/director">
-          Director
+          Director<?php if ( has_term ( 'director', 'portfolio_category' ) === FALSE ) { ?> <span class="portfolio-arrow-fix">&#8594;</span><?php } ?>
         </a>
       </div>
       <div class="col-xs-6 col-sm-2<?php if ( has_term ( 'teacher', 'portfolio_category' ) ) { ?> portfolio-active<?php } ?>">
         <a href="<?php get_bloginfo( ‘template_url’ ); ?>/past/teacher">
-          Teacher
+          Teacher<?php if ( has_term ( 'teacher', 'portfolio_category' ) === FALSE ) { ?> <span class="portfolio-arrow-fix">&#8594;</span><?php } ?>
         </a>
       </div>
     </div>
