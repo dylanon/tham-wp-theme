52dd5fcb7ae868e69356624fd7d2cf529d45125a
on.dylan <on.dylan@gmail.com>
2017-01-23 23:41:29 -0500

    Mobile portfolio nav styling

--- nav-portfolio.php
+++ nav-portfolio.php
@@ -8,17 +8,25 @@
         </p>
       </div>
 <!--       <div class="col-sm-2"></div> -->
-      <div class="col-xs-6 col-sm-2">
-        <a href="<?php get_bloginfo( ‘template_url’ ); ?>/past/actor">Actor</a>
+      <div class="col-xs-6 col-sm-2<?php if ( has_term ( 'actor', 'portfolio_category' ) ) { ?> portfolio-active<?php } ?>">
+        <a href="<?php get_bloginfo( ‘template_url’ ); ?>/past/actor">
+          Actor
+        </a>
       </div>
-      <div class="col-xs-6 col-sm-2">
-        <a href="<?php get_bloginfo( ‘template_url’ ); ?>/past/creator">Creator</a>
+      <div class="col-xs-6 col-sm-2<?php if ( has_term ( 'creator', 'portfolio_category' ) ) { ?> portfolio-active<?php } ?>">
+        <a href="<?php get_bloginfo( ‘template_url’ ); ?>/past/creator">
+          Creator
+        </a>
       </div>
-      <div class="col-xs-6 col-sm-2">
-        <a href="<?php get_bloginfo( ‘template_url’ ); ?>/past/director">Director</a>
+      <div class="col-xs-6 col-sm-2<?php if ( has_term ( 'director', 'portfolio_category' ) ) { ?> portfolio-active<?php } ?>">
+        <a href="<?php get_bloginfo( ‘template_url’ ); ?>/past/director">
+          Director
+        </a>
       </div>
-      <div class="col-xs-6 col-sm-2">
-        <a href="<?php get_bloginfo( ‘template_url’ ); ?>/past/teacher">Teacher</a>
+      <div class="col-xs-6 col-sm-2<?php if ( has_term ( 'teacher', 'portfolio_category' ) ) { ?> portfolio-active<?php } ?>">
+        <a href="<?php get_bloginfo( ‘template_url’ ); ?>/past/teacher">
+          Teacher
+        </a>
       </div>
     </div>
   </div>
