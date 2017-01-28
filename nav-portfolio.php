137454083821b58e2457ec776754de5d246186da
on.dylan <on.dylan@gmail.com>
2017-01-27 20:15:56 -0500

    Remove 'View project as' & center portfolio nav

--- nav-portfolio.php
+++ nav-portfolio.php
@@ -4,27 +4,22 @@
 <nav class="nav-portfolio">
   <div class="container">
     <div class="row text-center">
-      <div class="col-xs-12 col-sm-4">
-        <p>
-          View projects as:
-        </p>
-      </div>
-      <div class="col-xs-6 col-sm-2<?php if ( activePortfolioNav( 'actor', $portfoliosection ) ) { ?> portfolio-active<?php } ?>">
+      <div class="col-xs-6 col-sm-3<?php if ( activePortfolioNav( 'actor', $portfoliosection ) ) { ?> portfolio-active<?php } ?>">
         <a href="<?php get_bloginfo( ‘template_url’ ); ?>/past/actor">
           Actor<?php if ( activePortfolioNav( 'actor', $portfoliosection ) == FALSE ) { ?> <span class="portfolio-arrow-fix">&#8594;</span><?php } ?>
         </a>
       </div>
-      <div class="col-xs-6 col-sm-2<?php if ( activePortfolioNav( 'creator', $portfoliosection ) ) { ?> portfolio-active<?php } ?>">
+      <div class="col-xs-6 col-sm-3<?php if ( activePortfolioNav( 'creator', $portfoliosection ) ) { ?> portfolio-active<?php } ?>">
         <a href="<?php get_bloginfo( ‘template_url’ ); ?>/past/creator">
           Creator<?php if ( activePortfolioNav( 'creator', $portfoliosection ) == FALSE ) { ?> <span class="portfolio-arrow-fix">&#8594;</span><?php } ?>
         </a>
       </div>
-      <div class="col-xs-6 col-sm-2<?php if ( activePortfolioNav( 'director', $portfoliosection ) ) { ?> portfolio-active<?php } ?>">
+      <div class="col-xs-6 col-sm-3<?php if ( activePortfolioNav( 'director', $portfoliosection ) ) { ?> portfolio-active<?php } ?>">
         <a href="<?php get_bloginfo( ‘template_url’ ); ?>/past/director">
           Director<?php if ( activePortfolioNav( 'director', $portfoliosection ) == FALSE ) { ?> <span class="portfolio-arrow-fix">&#8594;</span><?php } ?>
         </a>
       </div>
-      <div class="col-xs-6 col-sm-2<?php if ( activePortfolioNav( 'teacher', $portfoliosection ) ) { ?> portfolio-active<?php } ?>">
+      <div class="col-xs-6 col-sm-3<?php if ( activePortfolioNav( 'teacher', $portfoliosection ) ) { ?> portfolio-active<?php } ?>">
         <a href="<?php get_bloginfo( ‘template_url’ ); ?>/past/teacher">
           Teacher<?php if ( activePortfolioNav( 'teacher', $portfoliosection ) == FALSE ) { ?> <span class="portfolio-arrow-fix">&#8594;</span><?php } ?>
         </a>
