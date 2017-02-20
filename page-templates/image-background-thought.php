63b0881cb8a1750032126b264c228fae29e3abad
on.dylan <on.dylan@gmail.com>
2017-02-19 19:31:38 -0500

    Always centre page titles

--- page-templates/image-background-thought.php
+++ page-templates/image-background-thought.php
@@ -8,7 +8,7 @@
       
       <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-        <h1><?php the_title(); ?></h1>
+        <h1 class="text-center"><?php the_title(); ?></h1>
       
         <?php the_content(); ?>
         <?php edit_post_link(); ?>
